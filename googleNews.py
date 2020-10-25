1597f9b06a8b13886d026e39cb12026a4d50ee6d
Sean <snhartrich@gmail.com>
2020-10-24 20:44:46 -0700

    Conform to new api limits
    Conform to new api limits

--- googleNews.py
+++ googleNews.py
@@ -10,10 +10,26 @@ load_dotenv()
 current_directory = os.path.dirname(os.path.abspath(__file__))
 news_directory = '/data/news'
 
-KEY_1 = os.getenv('KEY_1')
-KEY_2 = os.getenv('KEY_2')
-KEY_3 = os.getenv('KEY_3')
-KEY_4 = os.getenv('KEY_4')
+TEST_RUN = os.getenv('TEST_RUN')
+KEYS = []
+KEYS.append(os.getenv('KEY_0'))
+KEYS.append(os.getenv('KEY_1'))
+KEYS.append(os.getenv('KEY_2'))
+KEYS.append(os.getenv('KEY_3'))
+KEYS.append(os.getenv('KEY_4'))
+KEYS.append(os.getenv('KEY_5'))
+
+SOURCE_CHUNK_SIZE = os.getenv('SOURCE_CHUNK_SIZE')
+if not SOURCE_CHUNK_SIZE:
+    SOURCE_CHUNK_SIZE = 19
+
+
+def chunkify_array(array, chunk_size):
+    """Yield successive n-sized chunks from arr."""
+    arr_chunks = []
+    for i in range(0, len(array), chunk_size):
+        arr_chunks.append(array[i:i + chunk_size]) 
+    return arr_chunks
 
 
 def file_format_datetime(datetime_value):
@@ -24,16 +40,15 @@ def get_last_recorded_timestamp():
     if not past_files:
         return None
     iso_datetime_strings = list(map(lambda file_name: file_name.split('.')[0], past_files))
-
     return max(iso_datetime_strings)
 
 
 def get_sources():
-    source_url  =  'https://newsapi.org/v1/sources?language=en'
-    response  =  requests.get(source_url).json()
-    sources  =  []
+    source_url = 'https://newsapi.org/v1/sources?language=en'
+    response = requests.get(source_url).json()
+    sources = []
     for source in response['sources']:
-             sources.append(source['id'])
+        sources.append(source['id'])
     return sources
 
 def mapping():
@@ -50,31 +65,56 @@ def category(source, m):
     except:
         return 'NC'
 
-def get_daily_news(key):
-    to_datetime = datetime.now()
-    from_datetime = get_last_recorded_timestamp()
+def request_articles(url):
+    response = requests.get(url)
+    r = response.json()
+    try:
+        for article in r['articles']:
+            article['source'] = article['source']['id']
+        return r
+    except:
+        print('API Error...')
+        print(r)
+        return None
 
-    url = 'https://newsapi.org/v1/articles?pageSize=100&to=%s' % to_datetime.isoformat(timespec='seconds') + '&source={0}&sortBy={1}&apiKey={2}'
+
+def get_daily_news(key):
+    # to_datetime = datetime.now()
+    to_datetime = datetime.utcnow()
+    from_datetime = datetime.strptime(get_last_recorded_timestamp(), '%Y-%m-%dT%H%M%S').strftime('%Y-%m-%dT%H:%M:%S')
+
+    sources = get_sources()
+    sources_in_chunks = chunkify_array(sources, SOURCE_CHUNK_SIZE)
+    sources_strings = list(map(lambda x: ','.join(x), sources_in_chunks))
+    print(from_datetime, to_datetime)
+    # return
+    
+    base_url = 'https://newsapi.org/v2/everything?pageSize=100&to=%s' % to_datetime.isoformat(timespec='seconds') + '&sortBy={0}&apiKey={1}'
+    url = base_url.format('publishedAt', key)
     if from_datetime:
         url += '&from=%s' % from_datetime
 
     responses = []
-    sources = get_sources()
-    for i, source in enumerate(sources):
+    for source_string in sources_strings:
+        sources_url = url + '&sources=%s' % source_string
 
-        try:
-            u = url.format(source, 'top', key)
-        except:
-            u = url.format(source, 'latest', key)
-
-        response = requests.get(u)
+        response = requests.get(sources_url)
         r = response.json()
         try:
             for article in r['articles']:
-                article['source'] = source
+                article['source'] = article['source']['id']
             responses.append(r)
+
+            print(source_string)
+            print(r['totalResults'])
+
+            # TODO free api version caps results to 100, no pagination allowed
+            # if r['totalResults'] > 100:
+            #     for page_number in range(1, 2 + r['totalResults'] // 100 ):
+            #         responses.append(request_articles(sources_url + '&page=%s' % page_number))
         except:
-            print('Rate limit exceeded ... please wait and retry in 6 hours')
+            print('API Error...')
+            print(r)
             return None
 
     articles = list(map(lambda r: r['articles'], responses))
@@ -90,19 +130,19 @@ def get_daily_news(key):
     news['scraping_date'] = to_datetime
     
     if len(news.index) > 0:
-        save_path = current_directory + '/data/news/%s.csv' % file_format_datetime(to_datetime)
+        max_article_published_at = news['publishedAt'].max()
+        max_datetime = datetime.strptime(max_article_published_at, '%Y-%m-%dT%H:%M:%SZ')
+        save_path = current_directory + '/data/news/%s.csv' % file_format_datetime(max_datetime)
         news.to_csv(save_path, index=False, encoding='utf-8')
 
     print('Done', datetime.now(), len(news.index))
 
 n = datetime.now()
-h = n.hour % 4
+h = n.hour % 5
 
-if h == 1:
-    get_daily_news(KEY_1)
-elif h == 2:
-    get_daily_news(KEY_2)
-elif h == 3:
-    get_daily_news(KEY_3)
+if TEST_RUN:
+    print('KEY_0')
+    get_daily_news(KEYS[5])
 else:
-    get_daily_news(KEY_4)
+    print('KEY_%s' % h) 
+    get_daily_news(KEYS[h])
